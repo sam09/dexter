9f00b3ddf154c864837778d83eabba7e5550637f
Sam Radhakrishnan <sk09idm@gmail.com>
2016-09-22 18:38:46 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# geek-fetch package
+
+http://www.geeksforgeeks.org/basic-and-extended-euclidean-algorithms/
+An atom package to fetch code from geeks for geeks based on a particular search snippet
---
+++ spec/geek-fetch-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import GeekFetchView from '../lib/geek-fetch-view';
+
+describe('GeekFetchView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
